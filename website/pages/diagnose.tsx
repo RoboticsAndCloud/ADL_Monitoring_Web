e0961cc277980d3e201488329bd736bb84a1c36c
Your Name <you@example.com>
2023-10-04 21:19:34 -0500

    add diagnose.tsx

--- website/pages/diagnose.tsx
+++ website/pages/diagnose.tsx
@@ -6,6 +6,7 @@ import Field from '@components/Field';
 import * as Yup from 'yup';
 import axios from 'axios'
 import { useRefreshProps } from '@util/routerUtil';
+import AppLayout from '@components/AppLayout';
 
 const QuestionSchema = Yup.object().shape({
     content: Yup.string().min(1, 'Too short').required('Required'),
@@ -27,7 +28,7 @@ interface Props {
 }
 
 const DiagnoseMethod = ({ id, content, choices, correctAnswer}: Props) => {
-    const [open, setOpen] = useState(false)
+    const [open, setOpen] = useState(true)
     const { refresh } = useRefreshProps()
 
     const cancelButtonRef = useRef(null)
@@ -44,123 +45,130 @@ const DiagnoseMethod = ({ id, content, choices, correctAnswer}: Props) => {
 
 
 
-    return   ( 
-        <div className='container'>
-        <div className='bg-slate-100 h16 w-screen flex  justify-center'>
-        <h1 className='text-5xl text-cyan-700'>Bayesian Diagnose</h1>
-        </div>
+    return   <AppLayout>
+    
+    <div className=''>
+        <h1 className="text-4xl text-green-700 text-center font-semibold">AI Diagnose Platform</h1>
+        <div className="border-t-4 mb-2 mt-2 border-green-500 "></div>
 
-        <div className='justify-center items-center flex '>
-           <h1 className='text-3xl text-cyan-700 justify-center'>Your input:</h1>
-        </div>
-
-        <div className=" flex items-center justify-center w-screen border-solid">
-     
-            <div className='h 56 content-center border-solid border-2 border-indigo-600'>
-            <h1>Location: Kitchen</h1>
-            <h1>Objects: Table</h1>
-            <h1>Sound: Microwave</h1>
-            <h1>Motion: Sitting</h1>
-            </div>
-
-            <div>
-                <h1 className='text-3xl  text-indigo-600 hover:text-indigo-900'> => </h1>
-            </div>
-        
-        
-
-        {/* <div className=" flex items-center justify-center w-screen"> */}
-            <div className=''>
-
-            <button
-            type="button"
-            className="text-2xl  text-indigo-600 hover:text-indigo-900  border-solid border-2 border-green-500"
-            onClick={() => setOpen(true)}
-            >
-            Bayesian Diagnose
-
-            </button>
-            </div>
-        </div>
-        </div>
-    )
-
-{/*         
-        <Transition.Root show={open} as={Fragment}>
-            <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
-                <Transition.Child
-                    as={Fragment}
-                    enter="ease-out duration-300"
-                    enterFrom="opacity-0"
-                    enterTo="opacity-100"
-                    leave="ease-in duration-200"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
-                    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                </Transition.Child>
-
-                <div className="fixed z-10 inset-0 overflow-y-auto">
-                    <div className="flex items-end sm:items-center justify-center min-h-full p-4 text-center sm:p-0">
-                        <Transition.Child
-                            as={Fragment}
-                            enter="ease-out duration-300"
-                            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                            enterTo="opacity-100 translate-y-0 sm:scale-100"
-                            leave="ease-in duration-200"
-                            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        >
-                            <Dialog.Panel className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
-                                <Formik validationSchema={QuestionSchema} initialValues={{
-                                    id,
-                                    content,
-                                    choices: choices || '',
-                                    correctAnswer: correctAnswer || '',
-                                }} onSubmit={onSubmit}>
-                                    {() => (<Form>
-                                        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                                            <div className="sm:flex sm:items-start">
-                                                <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-indigo-100 sm:mx-0 sm:h-10 sm:w-10">
-                                                    <PlusCircleIcon className="h-6 w-6 text-indigo-600" aria-hidden="true" />
-                                                </div>
-                                                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                                    <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                                                        Update question
-                                                    </Dialog.Title>
-                                                    <div className="mt-2">
-                                                        <Field type="text" name="content" label="Content" />
-                                                        <Field type="text" name="choices" label="Choices (separate with |)" />
-                                                        <Field type="text" name="correctAnswer" label="Correct Answer" />
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                                            <button
-                                                type="submit"
-                                                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
-                                            >
-                                                Update
-                                            </button>
-                                            <button
-                                                type="button"
-                                                className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                                                onClick={() => setOpen(false)}
-                                                ref={cancelButtonRef}
-                                            >
-                                                Cancel
-                                            </button>
-                                        </div>
-                                    </Form>)}
-                                </Formik>
-                            </Dialog.Panel>
-                        </Transition.Child>
-                    </div>
+        {open &&
+                <div >
+                    Steps: Input your data, then Click Diagnose 
+                    {/* <button className="w-[15%] bg-indigo-700 font-semibold text-white py-1 px-1 rounded text-left mb-4">Input Raw Data</button>
+                    <button className="w-[20%] bg-green-500 font-semibold text-white py-1 px-1 rounded text-center mb-4"> ={'>'} </button>
+                    <button className="w-[40%] bg-indigo-700 font-semibold text-white py-1 px-1 rounded text-center mb-4">Recognition & Diagnose </button> */}
                 </div>
-            </Dialog>
-        </Transition.Root> */}
-    // </>
+        }
+
+        <button className="mt-2 w-[100%] bg-indigo-700 font-semibold text-white py-1 px-1 rounded text-left">Raw Data</button>
+
+        <div className="flex">
+            <div className="flex-1 w-32">
+                Image 
+                <img src="/image/20221001113420/image1.jpg" className="h-auto w-40" alt="Image " />
+            </div>
+            <div className="flex-1 w-32">
+                Audio
+                <img src="https://cdna.artstation.com/p/assets/images/images/017/295/280/large/dmitriy-novikov-1.jpg?1555408172" className="h-auto w-40" alt="Image " />
+            </div>
+            <div className="flex-1 w-32">
+                Motion
+                <img src="https://cdna.artstation.com/p/assets/images/images/017/295/280/large/dmitriy-novikov-1.jpg?1555408172" className="h-auto w-40" alt="Image " />
+            </div>
+        </div>
+
+
+
+        <div>
+            <button className="mt-4 mb-2 w-[100%] bg-indigo-700 font-semibold text-white py-1 px-1 rounded text-left"> AI Diagnose (Click to Run)</button>
+
+{/* 
+            <p> Objects: Cup(90%) </p>
+            <p> Sound: Microwave(90%) </p>
+            <p> Motion: Stand(95%) </p>  */}
+            <p className={`${1}? "bg-blue-400":"bg-red-400"`}>abc</p>
+            <p className={"bg-red-400"}>abc</p>
+
+            <div className="flex flex-row  justify-center items-center">
+                <table className="table-auto text-left border text-sm ">
+                <thead>
+                <tr>
+                    <th className="p-2 bg-indigo-400 border-b border-l">Type</th>
+                    <th className="p-2 bg-indigo-400 border-b border-l">Top1</th>
+                    <th className="p-2 bg-indigo-400 border-b border-l">Top2</th>
+                    <th className="p-2 bg-indigo-400 border-b border-l">Top3</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr className="odd:bg-gray-200 hover:!bg-stone-200">
+                    <td className="p-2 border-b border-l">Location</td>
+                    <td className="p-2 border-b border-l">Kitchen(90%)</td>
+                    <td className="p-2 border-b border-l">LivingRoom(1%)</td>
+                    <td className="p-2 border-b border-l">Bedroom(0.1%)</td>
+                </tr>
+                <tr className="odd:bg-gray-200 hover:!bg-stone-200">
+                    <td className="p-2 border-b border-l">Object</td>
+                    <td className="p-2 border-b border-l">Cup(90%)</td>
+                    <td className="p-2 border-b border-l">Microwave(90%)</td>
+                    <td className="p-2 border-b border-l">Table(90%)</td>
+                </tr>
+                <tr className="odd:bg-gray-200 hover:!bg-stone-200">
+                    <td className="p-2 border-b border-l">Sound</td>
+                    <td className="p-2 border-b border-l">Microwave(90%)</td>
+                    <td className="p-2 border-b border-l">Drinking(1%)</td>
+                    <td className="p-2 border-b border-l">Eating(0.1%)</td>
+                </tr>
+                <tr className="odd:bg-gray-200 hover:!bg-stone-200">
+                    <td className="p-2 border-b border-l">Motion</td>
+                    <td className="p-2 border-b border-l">Stand(90%)</td>
+                    <td className="p-2 border-b border-l">Walk(1%)</td>
+                    <td className="p-2 border-b border-l">Sit(0.1%)</td>
+                </tr>
+                </tbody>
+                </table>
+            </div>
+        </div>
+
+
+        <div>
+            <button className="mt-2 mb-1 w-[100%] bg-indigo-700 font-semibold text-white py-1 px-1 rounded text-left"> Bayesian Diagnose (Click to Run)</button>
+            <span className="bg-sky-400">Baysian Process:</span>
+            <img src="https://www.freecodecamp.org/news/content/images/2020/07/Screenshot-2020-07-19-at-22.58.48.png" />
+
+            <span className="bg-sky-400">Baysian Result (Top3):</span>
+            <div className="flex flex-row  justify-center items-center">
+                <table className="table-auto text-left border text-sm ">
+                <thead>
+                <tr>
+                    <th className="p-2 bg-indigo-400 border-b border-l">Activity</th>
+                    <th className="p-2 bg-indigo-400 border-b border-l">Probability</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr className="odd:bg-red-400 hover:!bg-stone-200">
+                    <td className="p-2 border-b border-l">Kitchen_Activity</td>
+                    <td className="p-2 border-b border-l">90%</td>
+                </tr>
+                <tr className="odd:bg-gray-200 hover:!bg-stone-200">
+                    <td className="p-2 border-b border-l">Reading</td>
+                    <td className="p-2 border-b border-l">0.8</td>
+                </tr>
+                <tr className="odd:bg-gray-200 hover:!bg-stone-200">
+                    <td className="p-2 border-b border-l">Watch TV</td>
+                    <td className="p-4 border-b border-l">0.1</td>
+                </tr>
+                </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div className="border-t-4 mb-2 mt-2 border-green-500 "></div>
+    </div>
+       
+    </AppLayout>
 }
 
 export default DiagnoseMethod
+
+
+
